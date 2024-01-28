487570fad5e8b405911e93f13158f0fbf8fcebc6
Natnael Abebe <natnaelabebe1994@gmail.com>
2024-01-29 02:19:49 +0300

    only roll dice that are not held

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,39 @@ function App() {
   function allNewDice() {
     let newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
-  const diceElements = dice.map((die) => <Die key={die.id} value={die.value} />);
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) => oldDice.map(die=> {
+      return die.isHeld? die : generateNewDie();
+    }));
   }
 
   return (
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Die(props) {
-  return <div className="die">{props.value}</div>;
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "#fff",
+  };
+  return (
+    <div className="die" style={styles} onClick={props.holdDice}>
+      {props.value}
+    </div>
+  );
 }
