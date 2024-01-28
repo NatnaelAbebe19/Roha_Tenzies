0e3069cfb0e26446b11d02cbdca2a65c6473092f
Natnael Abebe <natnaelabebe1994@gmail.com>
2024-01-28 23:32:41 +0300

    array of random numbers from 1 to 6 were created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,18 @@ import Die from "./components/Die";
 import "./App.css";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  const diceElements = dice.map((die) => <Die value={die} />);
+
   return (
     <div className="container">
       <div className="inside--container">
@@ -14,16 +26,7 @@ function App() {
           </p>
         </div>
         <div className="die--wrapper">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
+          {diceElements} 
         </div>
         <div className="roll">
           <button>Roll</button>
